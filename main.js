dd7b8fbc881c497de8964581cd7b8a0a9e38dadc
vlad-tircomnicu <tircomnicu@gmail.com>
2022-08-17 12:32:37 +0300

    Refactor and comment
    Refactor the Escape button and add comment to the code

--- main.js
+++ main.js
@@ -14,10 +14,21 @@ const
         overlay.classList.remove('hidden')
     }
 
+// show modal on any button click
 showModalButton.forEach((item) => {
     item.addEventListener('click', showModal)
 })
 
+// close modal on close button click
 closeModalButton.addEventListener('click', closeModal)
 
+// close modal on overlay click
 overlay.addEventListener('click', closeModal)
+
+// close modal on esc button press
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal()
+    }
+})
+
